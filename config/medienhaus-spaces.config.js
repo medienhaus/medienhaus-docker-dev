2e325f027bc7e9f2c6505784b58ce7d9215c3579
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-10-26 17:07:04 +0200

    build: update config/medienhaus-spaces.config.js for latest main
    for reference: medienhaus/medienhaus-spaces#60

--- config/medienhaus-spaces.config.js
+++ config/medienhaus-spaces.config.js
@@ -1,64 +1,27 @@
-const WebpackConfig = require('./webpack.config.js');
-
-// eslint-disable-next-line no-undef
 module.exports = {
-    publicRuntimeConfig: {
-        name: 'medienhaus/spaces',
-        authProviders: {
-            matrix: {
-                baseUrl: 'http://matrix.localhost',
-                allowCustomHomeserver: true,
-            },
-            etherpad: {
-                path: '/write',
-                baseUrl: 'http://etherpad.localhost/p',
-                myPads: {
-                    api: 'http://etherpad.localhost/mypads/api',
-                },
-            },
-            spacedeck: {
-                path: '/sketch',
-                baseUrl: 'http://spacedeck.localhost',
+    name: 'medienhaus/spaces',
+    authProviders: {
+        matrix: {
+            baseUrl: 'http://matrix.localhost',
+            allowCustomHomeserver: true,
+        },
+        etherpad: {
+            path: '/write',
+            baseUrl: 'http://etherpad.localhost/p',
+            myPads: {
+                api: 'http://etherpad.localhost/mypads/api',
             },
         },
-        account: {
-            allowAddingNewEmails: false,
+        spacedeck: {
+            path: '/sketch',
+            baseUrl: 'http://spacedeck.localhost',
         },
-        chat: {
-            pathToElement: 'http://localhost/element',
-        },
-        contextRootSpaceRoomId: '',
     },
-    rewrites() {
-        const rewriteConfig = [];
-
-        if (this.publicRuntimeConfig.authProviders.etherpad) {
-            rewriteConfig.push({
-                source: this.publicRuntimeConfig.authProviders.etherpad.path,
-                destination: '/etherpad',
-            },
-            {
-                source: this.publicRuntimeConfig.authProviders.etherpad.path + '/:roomId',
-                destination: '/etherpad/:roomId',
-            });
-        }
-
-        if (this.publicRuntimeConfig.authProviders.spacedeck) {
-            rewriteConfig.push({
-                source: this.publicRuntimeConfig.authProviders.spacedeck.path,
-                destination: '/spacedeck',
-            },
-            {
-                source: this.publicRuntimeConfig.authProviders.spacedeck.path + '/:roomId',
-                destination: '/spacedeck/:roomId',
-            });
-        }
-
-        return rewriteConfig;
+    account: {
+        allowAddingNewEmails: false,
     },
-    compiler: {
-        styledComponents: true,
+    chat: {
+        pathToElement: 'http://localhost/element',
     },
-    output: 'standalone',
-    webpack: WebpackConfig,
+    contextRootSpaceRoomId: process.env.MEDIENHAUS_ROOT_CONTEXT_SPACE_ID,
 };
