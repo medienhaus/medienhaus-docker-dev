725a7382985aa72eb9e16042546aac710ae70a2a
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-11-28 13:15:50 +0100

    chore: add some comments to scripts/reset.sh

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -4,7 +4,11 @@ set -euo pipefail
 
 trap "printf \"\n-- %s -- \n\n\" \"$0: was interrupted\" >&2; exit 2" INT TERM
 
+# -- make sure the docker composition is shut down -----------------------------
+
 docker compose down
+
+# -- remove previously stored data of various services -------------------------
 
 rm -rf data/etherpad
 
@@ -14,9 +18,15 @@ rm -rf data/matrix-synapse
 
 rm -rf data/spacedeck
 
+# -- start docker composition, and make sure to build and force-recreate -------
+
 docker compose up -d --build --force-recreate --wait
 
+# -- run the initialisation script; see comments in the script itself ----------
+
 ./scripts/init.sh
+
+# -- re-build and force-recreate medienhaus-spaces after initialisation --------
 
 docker compose up -d --build --force-recreate --wait medienhaus-spaces
 
