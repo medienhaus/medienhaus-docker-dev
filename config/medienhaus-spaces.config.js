f9f9b079725f9e17af6aa7ca06a11b44c72de609
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-04-18 11:58:15 +0200

    update medienhaus-spaces config template

--- config/medienhaus-spaces.config.js
+++ config/medienhaus-spaces.config.js
@@ -1,31 +1,47 @@
 module.exports = {
-    name: 'medienhaus/spaces',
-    authProviders: {
-        matrix: {
-            baseUrl: 'http://matrix.localhost',
-            allowCustomHomeserver: true,
-        },
-        etherpad: {
-            path: '/write',
-            baseUrl: 'http://etherpad.localhost/p',
-            myPads: {
-                api: 'http://etherpad.localhost/mypads/api',
-            },
-        },
-        spacedeck: {
-            path: '/sketch',
-            baseUrl: 'http://spacedeck.localhost',
-        },
-        tldraw: {
-            path: '/draw',
-        },
+  name: "medienhaus/spaces",
+  authProviders: {
+    matrix: {
+      baseUrl: "http://matrix.localhost",
+      allowCustomHomeserver: true,
     },
-    account: {
-        allowAddingNewEmails: false,
+    etherpad: {
+      path: "/write",
+      baseUrl: "http://etherpad.localhost/p",
+      myPads: {
+        api: "http://etherpad.localhost/mypads/api",
+      },
     },
-    chat: {
-        pathToElement: 'http://localhost/element',
+    spacedeck: {
+      path: "/sketch",
+      baseUrl: "http://spacedeck.localhost",
     },
-    contextRootSpaceRoomId: process.env.MEDIENHAUS_ROOT_CONTEXT_SPACE_ID,
-    templates: { item: ['etherpad', 'spacedeck', 'tldraw', 'studentproject', 'link'] },
+    tldraw: {
+      path: "/draw",
+    },
+  },
+  account: {
+    allowAddingNewEmails: false,
+  },
+  chat: {
+    pathToElement: "http://localhost/element",
+  },
+  contextRootSpaceRoomId: process.env.MEDIENHAUS_ROOT_CONTEXT_SPACE_ID,
+  /* @NOTE: clear(!) client-side localStorage if value defined in `versionToken` is */
+  /* *not_equal* to value stored in or does *not_exist* in client-side localStorage */
+  // localStorage: {
+  //   clearAfterUpgrade: true,
+  //   versionToken: "1",
+  // },
+  templates: {
+    // context: [
+    //   "context",
+    // ],
+    item: [
+      "etherpad",
+      "spacedeck",
+      "tldraw",
+      "link",
+    ],
+  },
 };
